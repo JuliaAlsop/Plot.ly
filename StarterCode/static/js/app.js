180201f92b03451166da96d13b4e5ea200cc8a01
Julia Alsop <64661590+JuliaAlsop@users.noreply.github.com>
2020-09-23 23:29:46 -0400

    Completion of part 1 and 2 of hw

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,49 +1,49 @@
-// Fetch the JSON data and console log it
+//Part 1: Fetch the JSON data and console log it
+
 d3.json("data/samples.json").then((importedData) => {
     console.log("importedData");
-    var data = importedData;
+    var Data = importedData;
 
-//Build your select dropdown based on contents for 
-    var data = Object.values(data.names);
+// Part 2: Creating Horizontal Bar Chart 
 
-//Filter the demographic info based on the select
-    var labels = Object.keys(data.names);
-})
+// Sort the data by Name
+var sortedname = Data.sort((a, b) => b.name - a.name);
 
-// Promise Pending
-  const dataPromise = d3.json("samples.json");
-  console.log("Data Promise: ", dataPromise);
+// Slice the first 10 objects for plotting
+slicedData = sortedname.slice(0, 10);
 
-// Create a horizontal bar chart with dropdown menu to display top 10 
-// OTUs found in that individual 
-function init() {
-    var data = [{
-      values: us,
-      labels: labels,
-      type: "pie"
-    }];
-  
-    var layout = {
-      height: 600,
-      width: 800
-    };
-  
-    Plotly.newPlot("pie", data, layout);
+// Reverse the array to accommodate Plotly's defaults
+reversedData = slicedData.reverse();
+
+// Trace1 for the OTU Data
+var trace1 = {
+  x: reversedData.map(object => object.sample_values),
+  y: reversedData.map(object => object.otu_ids),
+  text: reversedData.map(object => object.sample_values),
+  name: "Data",
+  type: "bar",
+  orientation: "h"
+};
+
+// data
+var data = [trace1];
+
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Top 10 OTUs found in Individual",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
   }
-  
-function optionChanged(value) {
-    //run chart for selected data with d3;
-    metadata = mydata.metadata.filter(/* Find element with matching 
-        id of value */)
-    plotObject = mydata.samples.filter(/* Find sample with id
-        === value */)
+};
 
-    sorttableData = plotObject.otu_ids.map((obj, index))
-       {
-        return {
-            otu_id: obj,
-            sample_value: plotObject.sample_values
-            [index]
-        }
-    }
-}
+// Render the plot to the div tag with 
+Plotly.newPlot("plot", data, layout);
+
+//Part 3: Create a bubble chart that displays each sample
+
+// Part 4: Display the sample metadata 
+
+//Part 5 (advanced) Plot Washing Frequency 
